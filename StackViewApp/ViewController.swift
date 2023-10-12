39539da5280109db5bf508cc6327d9717b708c8d
Muslim Gapparov <muslimgapparov@gmail.com>
2023-10-12 21:33:27 +0300

    final version of traffic light

--- StackViewApp/ViewController.swift
+++ StackViewApp/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
     }
 
-
+    override func viewWillLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = redView.frame.width / 2
+        greenView.layer.cornerRadius = redView.frame.width / 2
+    }
+    
+    @IBAction func startButtonDidTapped() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            redView.alpha = lightIsOn
+            greenView.alpha = lightIsOff
+            currentLight = .yellow
+        case .yellow:
+            yellowView.alpha = lightIsOn
+            redView.alpha = lightIsOff
+            currentLight = .green
+        case .green:
+            greenView.alpha = lightIsOn
+            yellowView.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
+    
 }
 
